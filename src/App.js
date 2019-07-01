85d92715dbf8c946dc6cf11e95f57dd13b5fa73e
whosAle <abelgrave95@gmail.com>
2019-07-01 09:15:40 -0400

    imported data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import studentsData from './data/students.json';
+
+const App = () => {
+  console.log(studentsData);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Dashboard</h1>
     </div>
   );
 }
